380b84615df35afe5a52784e7dee2254bf8a1c7f
younver <younver48@gmail.com>
2022-12-30 14:38:06 +0300

    added surprise me

--- app/src/main/java/com/meri/recipe_app/MainActivity.java
+++ app/src/main/java/com/meri/recipe_app/MainActivity.java
@@ -5,23 +5,39 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import com.meri.recipe_app.chef_recipe.ChefRecipe;
+import com.meri.recipe_app.chef_recipe.ChefRecipeDisplayActivity;
 import com.meri.recipe_app.chef_recipe.ChefRecipeListActivity;
+import com.meri.recipe_app.database.RecipeDatabase;
+import com.meri.recipe_app.recipe.Recipe;
+import com.meri.recipe_app.recipe.RecipeDisplayActivity;
 import com.meri.recipe_app.recipe.RecipeListActivity;
 import com.meri.recipe_app.shopping_list.ShoppingListActivity;
 import com.meri.recipe_app.utils.Admin;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+    RecipeDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        db = Room.databaseBuilder(MainActivity.this,
+                RecipeDatabase.class,"recipe-database5").allowMainThreadQueries().build();
 
         final ActivityResultLauncher<Intent> launcher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
             @Override
@@ -42,6 +58,33 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, ChefRecipeListActivity.class);
+                launcher.launch(intent);
+            }
+        });
+
+        Button btnSurpriseMe = findViewById(R.id.btnSurpriseMain);
+        btnSurpriseMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ChefRecipeDisplayActivity.class);
+                Bundle bundle = new Bundle();
+                Random random = new Random();
+
+                List<ChefRecipe> recipes = db.chefRecipeDAO().getAllRecipes();
+                // error handler
+                if (recipes.size() <= 0) {
+                    Toast.makeText(MainActivity.this, "No chef recipe found", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                ChefRecipe recipe = recipes.get(random.nextInt(recipes.size()));
+
+                bundle.putString("recipeName",recipe.getName());
+                bundle.putStringArrayList("recipeMakingOf",recipe.getMakingOf());
+                bundle.putStringArrayList("recipeIngredients",recipe.getIngredients());
+                bundle.putParcelable("recipeImage", recipe.getImage());
+                intent.putExtras(bundle);
+
                 launcher.launch(intent);
             }
         });
